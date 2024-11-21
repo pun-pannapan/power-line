759106fdaee8efbf4a7c5aaca9e4b6350c41aabc
LAPTOP-HE6BAQAH\NWL-Pannapan-Laptop <pannapantu@gmail.com>
2024-11-21 11:12:40 +0700

    add line message api project

---
+++ Line.MessageApi/Global.asax.cs
@@ -0,0 +1,12 @@
+using System.Web.Http;
+
+namespace Line.MessageApi
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+        }
+    }
+}
